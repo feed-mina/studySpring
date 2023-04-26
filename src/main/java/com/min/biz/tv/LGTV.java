0d3071ec8c1e0bc0ee7a9a0b988de293cc54a324
feed-mina <myelin24@naver.com>
2023-04-26 12:17:02 +0900

    @component 어노테이션을 통해 IoC제어

--- src/main/java/com/min/biz/tv/LGTV.java
+++ src/main/java/com/min/biz/tv/LGTV.java
@@ -1,5 +1,14 @@
 package com.min.biz.tv;
 
+import org.springframework.stereotype.Component;
+
+// 마치 xml에서 <bean class="com.min.biz.tv.LGTV#0"/> 를 사용한것과 @Component 사용이 같은 역할을 한다.
+// 클래스 이름이 변경할때 컴포넌트만 인식하면 되기 때문에 xml을 따로 수정하지 않아도 되는 편리함이 있다.
+// 클래스의 이름을 기준으로 첫글자를 소문자로 변경하여 id로 인식한다.
+// 만약에 class이름이 복잡할 경우,  @("id이름")을 통해 설정할 수 있다.
+// @Component("tv")
+
+@Component("tv")
 public class LGTV implements TV {
 	public LGTV() {
 		System.out.println("=====>LGTV생성");
@@ -18,3 +27,5 @@ public class LGTV implements TV {
 	}
 
 }
+
+// @component 컴포넌트로 된 어노테이션으로 설정한 클래스를 context:component-scan 태그를 사용해서 발견하고 사용할수있다.
--- src/main/java/com/min/biz/tv/TVUser.java
+++ src/main/java/com/min/biz/tv/TVUser.java
@@ -3,36 +3,26 @@ package com.min.biz.tv;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 // 다형성 = 상속 + Overriding + 형변환
-// 1. 상속 인터페이스일때 implements, class라면 extends로 상속한다.
-// 2. 다른 클래스에도 동일하게 매서드를 맞춰 오버라이딩한다.
-// 3. 묵시적 형변환 : 자식객체가 부모타입 객체에 할당하는것 (영어로 프로모션이라 부른다.)
-// 다형성을 사용하는 이유 : 유지보수가 쉬워지기 때문에 !
-// 유지보수가 편하려면 다형성을 사용해야 한다.
+
 
 public class TVUser {
 	
 	public static void main(String[] args) {
-	
 		// 1. 스프링 컨테이너를 생성(구동)한다.
-		GenericXmlApplicationContext container = new GenericXmlApplicationContext("applicationContext.xml");
-		
+				GenericXmlApplicationContext container = new GenericXmlApplicationContext("applicationContext.xml");
+				
 		// 2. 스프링 컨테이너로부터 테스트할 객체를 검색(LookUp)한다.
-		 TV tv = (TV) container.getBean("tv");
-	
-		 // 디자인 패턴 적용	
-		// 마우스 오른쪽 클릭 > Run as > Run configuration > arguments > lg 또는 samsung
-	
-		 //		BeanFactory factory = new BeanFactory();
-		 //		TV tv = (TV) factory.getBean(args[0]);
-	
-		 //	 TV tv = new SamsungTV();
-		 tv.powerOn();
-		 tv.powerOff();
-		 tv.volumeUp();
-		 tv.volumeDown();
-		 
-		 container.close();
-
+				 TV tv = (TV) container.getBean("tv");
+			
+				 
+		// 스프링 컨테이너를 종료한다.  (컨테이너는 종료 직전에 자신이 생성한 객체들을 제거한다.)
+				 tv.powerOn();
+				 tv.powerOff();
+				 tv.volumeUp();
+				 tv.volumeDown();
+				 
+				 container.close();
 	}
 
 }
+// 스프링컨테이너 설정할때 xml설정파일을 등록하여 로딩한다. xml파일을 통해서 스프링컨테이너가 동작한다. 
