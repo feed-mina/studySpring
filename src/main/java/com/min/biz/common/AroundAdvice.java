c16640d40c9976ae5f94a7bc1de1ea780c675693
feed-mina <myelin24@naver.com>
2023-04-27 19:52:15 +0900

    aop:aspect 속성 - aop:around [20230427]

--- src/main/java/com/min/biz/common/AroundAdvice.java
+++ src/main/java/com/min/biz/common/AroundAdvice.java
@@ -2,6 +2,7 @@ package com.min.biz.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StopWatch;
 
 
 // serviceImpl 위에는 @Service , DAO 위에는 @repository , aroundadvice 객체를 @service 어노테이션을 id를 around로 줘서 bean 등록을 하지 않고 가능하게 해보자.
@@ -13,10 +14,15 @@ public class AroundAdvice {
 		Object obj = null;
 		System.out.println("----[Before Logic]---"); 
 
+		StopWatch watch = new StopWatch();
+		watch.start();
+		
+		
 		// 실질적으로 클라이언트가 호출한 비즈니스 메소드 실행
 		obj = jp.proceed();
 		
-		System.out.println("----[After Logic]---"); 
+		watch.stop();
+		System.out.println("비즈니스 메소드 수행에 소요된 시간 :" + watch.getTotalTimeSeconds()); 
 		return obj;
 	}
 
